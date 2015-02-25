1b27aa1819522ebd8c8cd0f916885c3b9250be13
Matt Kelliher <mattkelliher@gmail.com>
2015-02-24 21:52:33 -0500

    Start with a basic OP sword... inspired by Deanna at CRDL

--- src/main/java/com/jackhamma07/helloworld/HelloWorld.java
+++ src/main/java/com/jackhamma07/helloworld/HelloWorld.java
@@ -1,37 +1,52 @@
 package com.jackhamma07.helloworld;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 @Mod(modid = "HelloWorld", name = "Hello World", version = "1.7.2-1.0")
 public class HelloWorld {
 
-    //Define our custom item
-    public static Item LapisSword;
-
     // The instance of your mod that Forge will use
     @Mod.Instance("HelloWorld")
     public static HelloWorld instance;
+
+    //Define a custom material
+    //addToolMaterial(CUSTOMMATERIALNAME, harvestLevel, durability, miningSpeed, damageVsEntities, enchantability);
+    public static Item.ToolMaterial SWORD = EnumHelper.addToolMaterial("SWORD", 0, 10, 0, 20, 22);
+    //Assign it to our sword object
+    public static final ItemSword LapisSword = new LapisSword("LapisSword", SWORD);
 
     //This is the pre-init phase of loading your mod by Forge
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         //pre init stuff
 
-        //Tell forge about our Item
-        LapisSword = new LapisSword(); //create an instance of the item
         GameRegistry.registerItem(LapisSword, "LapisSword"); //register the item in Minecraft
+        LapisSword.setTextureName("HelloWorld" + ":" + "LapisSword");
     }
 
     //This is the init phase of loading your mod by Forge
     @Mod.EventHandler
     public void load(FMLInitializationEvent event) {
         //init stuff here
+
+        //Now create the shaped-crafting recipe for the LapisSword:
+
+
+        //ShapedOre Version that utilizes Forge's Ore Dictionary! Makes it compatible with other Mods!!
+        //GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(HelloWorld.LapisSword), " l ", " l ", " s ", "stickWood"));
+        //Ore Dictionary located at: http://www.minecraftforge.net/wiki/Common_Oredict_names
+        //this.setDamage(new ItemStack(LapisSword),100);
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(LapisSword), "ool", "edo", "seo", 'l', "gemLapis", 'd', "gemDiamond", 'e', "gemEmerald", 's', "stickWood", 'o', new ItemStack(Blocks.obsidian)));
     }
 
     //This is the post init phase of loading your mod by Forge
--- src/main/java/com/jackhamma07/helloworld/LapisSword.java
+++ src/main/java/com/jackhamma07/helloworld/LapisSword.java
@@ -1,14 +1,20 @@
 package com.jackhamma07.helloworld;
 
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
 
-public class LapisSword extends Item {
+public class LapisSword extends ItemSword {
 
-        public LapisSword() {
-            maxStackSize = 1;
-            setCreativeTab(CreativeTabs.tabMisc);
-            setUnlocalizedName("LapisSword");
+        public LapisSword(String unlocalizedName, ToolMaterial material) {
+            super(material);
+            this.setUnlocalizedName(unlocalizedName);
+            this.maxStackSize = 1;
+            this.setMaxDamage(20);
+            this.setCreativeTab(CreativeTabs.tabMisc);
+
         }
+
 
 }
